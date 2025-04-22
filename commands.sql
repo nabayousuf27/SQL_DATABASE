aae755636a5c47f32fa5f04fb2f0a98fd6a619d6
nabayousuf27 <nabay0519@gmail.com>
2025-04-22 10:36:03 +0500

    added more SQL commands

--- commands.sql
+++ commands.sql
@@ -11,3 +11,44 @@ Insert into student
 values
 ("naba" , 231020 , "computer");
 select * from student;
+
+create database  IF NOT EXISTS college;
+create database  IF NOT EXISTS university;
+drop database IF EXISTS university; 
+show databases;
+show tables;
+-- table queries 
+-- creating user table
+create database if not exists instagram ;
+use instagram;
+create table user(
+id int primary key,
+name varchar(50),
+email varchar(50),
+followers int ,
+following int 
+);
+INSERT INTO user
+values
+(1, "Naba" ,"nabay0519@gmaail.com",999 , 50),
+(2, "Mishaal" ,"Mishaal0519@gmaail.com",800 , 10),
+(3, "laiba" ,"laiba0519@gmaail.com",700 , 40);
+SHOW tables;
+SELECT * FROM user;
+
+-- constrains
+-- NOT NULL , UNIQUE ,  DEFAULT , CHECK
+-- Creating Table With Constrain
+
+create table user2(
+id int PRIMARY KEY ,
+namme varchar(50) NOT NULL,
+email varchar(50) unique,
+followers INT DEFAULT 0,
+following INT,
+CONSTRAINT CHECK(age >= 13)
+);
+
+
+
+
